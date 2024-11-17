51a2dd92797e087aee55ba901ed9358348fc588e
Anıl Şülekoğlu <39844117+sulek1201@users.noreply.github.com>
2024-11-17 10:22:26 +0300

    initiate credit application project

---
+++ credit/src/test/java/com/inghubs/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inghubs.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
